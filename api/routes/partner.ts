2e216b3c00f5a68bb4535361aa39e19ce6d88dcd
danyfahed7 <[dany.fahed1@gmail.com]>
2021-06-06 20:06:49 +0300

    optimizing fetch function

--- api/routes/partner.ts
+++ api/routes/partner.ts
@@ -7,7 +7,7 @@ class Partner {
     public express: express.Application;
     public logger: Logger;
 
-    // array to hold partners
+    // array to hold partners in range
     public partners: any[];
 
     constructor() {
@@ -31,72 +31,55 @@ class Partner {
             this.partners = []
             var partnersInRange = []
             const { greatCircleDistance } = require("great-circle-distance");
-            var range = Number(req.params.range);
-            var root = req.params.root;
             const fs = require('fs');
 
-            let rawdata = fs.readFileSync('./data/partners.json');
-            let partners = JSON.parse(rawdata);            
-            
-            for (var i = 0; i < partners.length; i++){
-                var partner = partners[i];
-                
-                for (var key in partner){
-                    var value = partner[key];                  
-                    var offices = partner["offices"];
-                    if(key=="offices"){
-                        for (var j = 0; j < offices.length; j++){
-                            var office = offices[j];       
-                            for (var key2 in office){
-                                var value2 = office[key2];
-                                var coordinates = office["coordinates"]
-                                var partnerInRange = JSON.parse(JSON.stringify(partner));
-                                if(key2=="coordinates"){
-                                    const lat2 = coordinates.split(",")[0]
-                                    const lng2 = coordinates.split(",")[1]
-                                    const coords = {
-                                        lat1: root.split(',')[0],
-                                        lng1: root.split(',')[1],
-                                        lat2: lat2,
-                                        lng2: lng2
-                                    };
-                                    var distance = greatCircleDistance(coords);
-                                    var addressInRange = office["address"] ;        
-                                    
-                                    if(distance <= range){   
-                                        partnerInRange.distance = distance;
-                                        partnerInRange.addressInRange = addressInRange  
-                                        partnerInRange.lat = lat2 
-                                        partnerInRange.lng = lng2                                  
-                                        partnersInRange.push(partnerInRange);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    
-                }                
-            }
+            //get parameters sent in the request
+            var range = Number(req.params.range);
+            var root = req.params.root;
 
+            // read partners json
+            let rawdata = fs.readFileSync('./data/partners.json');
+            let partners = JSON.parse(rawdata);
+
+            //loop on the partners
+            for (var i = 0; i < partners.length; i++){        
+                var offices = partners[i]["offices"];
+                //loop on the offices of this partner
+                for (var j = 0; j < offices.length; j++){
+                    //clone the partner, to search for the partner based on each address
+                    //if we want in the future to search for the unique partners, then we can change it
+                    var partnerInRange = JSON.parse(JSON.stringify(partners[i]));
+                    //get coordinates of this office
+                    var coordinates = offices[j]["coordinates"]
+                    const lat2 = coordinates.split(",")[0]
+                    const lng2 = coordinates.split(",")[1]
+                    const coords = {
+                        lat1: root.split(',')[0],
+                        lng1: root.split(',')[1],
+                        lat2: lat2,
+                        lng2: lng2
+                    };
+                    //get the great circle distance between the root and this office
+                    var distance = greatCircleDistance(coords);        
+                    
+                    if(distance <= range){   
+                        //if distance in the range selected, then add it to the array of partners in range
+                        //we add some extra fields in the partner in range object so we can easily display it on the map
+                        partnerInRange.distance = distance;
+                        partnerInRange.addressInRange = offices[j]["address"]  
+                        partnerInRange.lat = lat2 
+                        partnerInRange.lng = lng2                                  
+                        partnersInRange.push(partnerInRange);
+                    }
+                }           
+            }
+            //sort the array of partners in range based on the name of the partner, order ascending
             partnersInRange.sort(function (a, b) {
                 return a.organization.localeCompare(b.organization);
             });
             this.partners = partnersInRange;
             res.json(this.partners);
         });
-
-        // request to get all the partners in the specified range
-        this.express.get("/partners/", (req, res, next) => {
-            // this.logger.info("url:::::" + req.url);
-            const partner = this.partners.filter(function (partner) {
-                console.log(1)
-                // if (req.params.userName === user.userName) {
-                //     return user;
-                // }
-            });
-            res.json(partner);
-        });
-
     }
 }
 
